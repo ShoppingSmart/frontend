81c7da85f387a76a8ea887d3c298da97c291bd22
Wesley Flôres <wesleyfloresterres@gmail.com>
2023-02-26 22:32:14 -0300

    fix: fix app height

--- app.vue
+++ app.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex flex-col h-screen overflow-hidden">
+  <div class="flex flex-col h-screen">
     <header class="w-full text-center border-b border-grey p-4">
       <LayoutHeader></LayoutHeader>
     </header>
 
-    <main class="flex-1">
+    <main class="flex-1 overflow-y-auto">
       <NuxtPage />
     </main>
 
