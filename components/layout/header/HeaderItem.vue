e465c34d9f3e4196b065c82e2514c399fe8cf445
Wesley Flôres <wesleyfloresterres@gmail.com>
2023-02-26 22:08:03 -0300

    feat: create header item component

---
+++ components/layout/header/HeaderItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <nuxt-link :to="link" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">
+    <slot></slot>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  props: {
+    link: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
