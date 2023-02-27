842ccd126410e6eca64d4b32908fd96c9077e8fd
Wesley Flôres <wesleyfloresterres@gmail.com>
2023-02-27 00:48:03 -0300

    feat: create order index screen

---
+++ pages/orders/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="overflow-y-hidden">
+    <div class="2xl:container 2xl:mx-auto">
+      <div class="lg:px-20 md:px-6 px-4 md:py-12 py-8">
+        <div class="flex justify-between items-center">
+          <h1 class="text-3xl lg:text-4xl font-semibold text-gray-800 text-left">Orders</h1>
+
+          <nuxt-link to="/orders/create">
+            <SharedMoreButton></SharedMoreButton>
+          </nuxt-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
